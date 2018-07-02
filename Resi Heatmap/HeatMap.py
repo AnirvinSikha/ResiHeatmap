c3ba0fdef5cd3fa6e5c908dc7993b564f320c238
Anirvin Sikha <anirvin.sikha@gmail.com>
2018-07-02 13:57:25 -0700

    heatmap problems

--- Resi Heatmap/HeatMap.py
+++ Resi Heatmap/HeatMap.py
@@ -1,7 +1,15 @@
-import geopandas as gpd
+import plotly.plotly as py
 import pandas as pd
-import folium
-from folium.plugins import HeatMap
+import plotly.figure_factory as ff
 
-dataset = gpd.read_file("table.csv")
-dataset.crs = ""
+#py.tools.set_credentials_file(username='ASikha', api_key='e9SmIFsps2jBwfKF0olK')
+fips = ['06021', '06023', '06027',
+        '06029', '06033', '06059',
+        '06047', '06049', '06051',
+        '06055', '06061']
+values = range(len(fips))
+
+fig = ff.create_choropleth(fips=fips, values=values)
+py.iplot(fig, filename='choropleth of some cali counties - full usa scope')
+fig = dict( data=data, layout=layout )
+py.iplot( fig, validate=False, filename='d3-world-map' )
