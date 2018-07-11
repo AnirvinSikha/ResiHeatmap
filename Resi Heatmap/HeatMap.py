6755e3c87642404934e25fb5f7134662a3594f03
Anirvin Sikha <anirvin.sikha@gmail.com>
2018-07-10 21:11:31 -0700

    working basemap code

--- Resi Heatmap/HeatMap.py
+++ Resi Heatmap/HeatMap.py
@@ -1,14 +1,25 @@
 import numpy as np
 from mpl_toolkits.basemap import Basemap
 import matplotlib.pyplot as plt
+import pandas as pd
 from datetime import datetime
 from netCDF4 import date2index
 from PIL import Image
 from netCDF4 import Dataset
 
+lat = [34, 32, 37]
+lon = [-118, -117, -121]
+#values = [1431, 1500, 1000]
+area = [1431, 50, 100]
+
+# data = pd.read_csv("table.csv")
+# lat = data["lat"].values
+# lon = data["lon"].values
+# values = data['values'].values
+# area = data['area'].values
 
 fig = plt.figure(figsize=(10, 8))
-m = Basemap(projection='cass', resolution='h',
+m = Basemap(projection='lcc', resolution='h',
             lat_0=37.5, lon_0=-119,
             width=1E6, height=1.2E6)
 m.drawcoastlines(color='black')
@@ -16,5 +27,15 @@ m.drawstates(color='black')
 m.drawrivers(color='blue')
 m.drawcounties(color='black')
 plt.title('Hello Cali')
+
+x, y = m(lon, lat)
+
+m.scatter(x, y, marker='o', color='r', s=area, cmap="Reds")
+
+for a in [100, 300, 500]:
+    plt.scatter([], [], c='k', alpha=0.5, s=a,
+                label=str(a) + ' km$^2$')
+plt.legend(scatterpoints=1, frameon=False,
+           labelspacing=1, loc='lower left');
 plt.show()
 m.drawcounties()
